22879ff821b3b4a4ae5bf054c69516cf5c831c73
Nam Hoang <hnam17397@gmail.com>
2023-07-29 11:17:55 +0700

    1

--- src/Service/auth.service.js
+++ src/Service/auth.service.js
@@ -2,7 +2,7 @@ import api from "./api";
 
 export const login =async (data)=>{
     try {   
-        const url="login-url";
+        const url="auth/admin/login";
         const rs = await api.post(url,data);
         return rs.data;
     } catch (error) {
