10232032eff8e82cc54740cd4b605d0afcdc1681
Nam Hoang <hnam17397@gmail.com>
2023-07-31 14:22:10 +0700

    1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React,{useReducer} from 'react';
 import reducer from './context/reducer';
@@ -10,16 +9,15 @@ import AdminLogin from "./Page/auth/login";
 import LayoutAdmin from "./Component/AdminLayout";
 
 
-const URL_ADMIN="/admin-adios";
 function App() {
   const [state,dispatch]=useReducer(reducer,STATE);
   return (
     <UserProvider value={{state,dispatch}}>
       <BrowserRouter>
         <Routes>
-          <Route  path={URL_ADMIN}  element={iAdmin(<AdminDashboard/>,false)}/>
-          <Route  path={URL_ADMIN+"/dashboard"} element={iAdmin(null,false)}/>
-          <Route  path={URL_ADMIN+"/login"}  element={iAdmin(<AdminLogin/>,true)}/>
+          <Route  path="/" exact id='1'  element={iAdmin(<AdminDashboard/>,false)}/>
+          <Route  path="/dashboard" element={iAdmin(null,false)}/>
+          <Route  path="/login"  element={iAdmin(<AdminLogin/>,true)}/>
           <Route  path='*' element={<div>Page Not FOUND - south</div>}/>
         </Routes>
       </BrowserRouter>
